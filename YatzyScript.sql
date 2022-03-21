e1ae41c7bfc0b898153deeff5f92b24bc413c0c1
UlrikFa <69585813+UlrikFa@users.noreply.github.com>
2022-03-21 13:24:16 +0100

    SQL script oppdatert etter databasemodell

--- YatzyScript.sql
+++ YatzyScript.sql
@@ -16,3 +16,46 @@ create table bruker
 
 insert into bruker(brukernavn, passord, fornavn, etternavn, email)
 values ('tester', 'tester123', 'teste', 'testesen', 't.testesen@gmail.com');
+
+create table yatzyspill
+(
+    spillid serial not null,
+    admin varchar not null,
+
+    constraint yatzyspill_pk primary key (spillid),
+
+    constraint bruker_fk foreign key (admin)
+    references Oblig3.bruker (brukernavn) match simple
+);
+
+create table spilldeltakelse
+(
+    spillid serial not null,
+    spillerid varchar not null,
+
+    constraint spilldeltakelse_pk primary key (spillid, spillerid),
+
+    constraint spillid_fk foreign key (spillid)
+    references Oblig3.yatzyspill (spillid) match simple,
+
+    constraint bruker_fk foreign key (spillerid)
+    references Oblig3.bruker (brukernavn) match simple
+);
+
+create table rundeoversikt
+(
+    rundenr smallint not null,
+    spillid serial not null,
+    spiller1score smallint,
+    spiller2score smallint,
+    spiller3score smallint,
+    spiller4score smallint,
+    spiller5score smallint,
+    spiller6score smallint,
+
+    constraint rundeoversikt_pk primary key (spillid, rundenr),
+
+    constraint spillid_fk foreign key (spillid)
+    references Oblig3.yatzyspill (spillid) match simple
+);
+
