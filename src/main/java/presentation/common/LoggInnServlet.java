780beeecbcc68ccb5ecd8ee2d170f848d224976b
UlrikFa <69585813+UlrikFa@users.noreply.github.com>
2022-03-24 17:19:43 +0100

    Lage bruker funker

--- src/main/java/presentation/common/LoggInnServlet.java
+++ src/main/java/presentation/common/LoggInnServlet.java
@@ -13,7 +13,7 @@ import businessLogic.common.Validator;
 
 //import presentation.common.DeltagerDAO;
 
-@WebServlet("/logginn")
+@WebServlet(name = "LoggInnServlet", urlPatterns ="/logginn")
 public class LoggInnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	//@EJB
