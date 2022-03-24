e56138a0ca79235f60f750ba3b47ccee5a58fbd2
UlrikFa <69585813+UlrikFa@users.noreply.github.com>
2022-03-24 16:29:46 +0100

    Opprette bruker basic funksj

--- src/main/java/presentation/common/Opprettbruker.java
+++ src/main/java/presentation/common/Opprettbruker.java
@@ -1,11 +1,16 @@
 package presentation.common;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import businessLogic.common.BrukerDAO;
+import model.common.Bruker;
 
 /**
  * Servlet implementation class Opprettbruker
@@ -13,14 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "Opprettbruker", urlPatterns= "/opprettbruker")
 public class Opprettbruker extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Opprettbruker() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	@EJB
+    private BrukerDAO bDAO;  
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,8 +33,13 @@ public class Opprettbruker extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		Bruker b = new Bruker();
+		b.setBrukernavn(request.getParameter("Brukernavn"));
+		b.setFornavn(request.getParameter("Fornavn"));
+		b.setEtternavn(request.getParameter("Etternavn"));
+		b.setEmail(request.getParameter("email"));
+		b.setPassord(request.getParameter("passord"));
+		bDAO.nyBruker(b);
 	}
 
 }
