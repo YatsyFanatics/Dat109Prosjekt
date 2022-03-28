d2f6656df4c0d9ba8f1dd0f7855143c4effd8fd3
Fiksdal <erikfix@hotmail.com>
2022-03-28 13:59:12 +0200

    merged feature: passordUtil

---
+++ src/main/java/businessLogic/test/PassordUtilTest.java
@@ -0,0 +1,33 @@
+package businessLogic.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import org.junit.jupiter.api.Test;
+
+import businessLogic.common.PassordUtil;
+
+public class PassordUtilTest {
+	PassordUtil passUtil = new PassordUtil();
+	
+	@Test
+	public void basicPassordTest() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		String pass = "passord123";
+		String hash = passUtil.generateStorngPasswordHash(pass);
+		Boolean validated = PassordUtil.validatePassword(pass, hash);
+		assertTrue(validated);
+	}
+	
+	@Test
+	public void feilPassordTest() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		String pass = "correcthorsebatterystaple";
+		String hash = passUtil.generateStorngPasswordHash(pass);
+		Boolean validated = PassordUtil.validatePassword("passord123", hash);
+		assertFalse(validated);
+	}
+	
+
+}
