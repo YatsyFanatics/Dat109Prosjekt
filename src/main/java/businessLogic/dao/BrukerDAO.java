93608de4dfe9c9634825c45ff09f1d60f6b4c886
594620 <Gingerbread.Dragon@gmail.com>
2022-04-03 15:02:52 +0200

    Update BrukerDAO.java

--- src/main/java/businessLogic/dao/BrukerDAO.java
+++ src/main/java/businessLogic/dao/BrukerDAO.java
@@ -41,10 +41,6 @@ public class BrukerDAO {
 		em.createNativeQuery("DELETE FROM Oblig3.bruker WHERE brukernavn = :brukernavn", Bruker.class).setParameter("brukernavn", brukernavn); 
 //bruker med stor bokstav muligens? ogs� usiker p� om "bruker.class" skal v�re der
 	}
-  
-public List<Bruker> hentAlleBrukere() {
-		return em.createQuery("select d from Bruker d", Bruker.class).getResultList();
-	}
 	
 	//flere metoder etter behov
 }
