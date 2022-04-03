c40a356b9a9bb9128170da6a9b36049a0b8979d6
Endre <69849510+594630@users.noreply.github.com>
2022-04-03 14:52:24 +0200

    Update IndexController.java

--- src/main/java/presentation/spillside/IndexController.java
+++ src/main/java/presentation/spillside/IndexController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import businessLogic.dao.BrukerDAO;
 import model.Bruker;
 import businessLogic.utils.LoggInnUtil;
@@ -18,27 +17,23 @@ import businessLogic.utils.LoggInnUtil;
 @WebServlet(name = "IndexController", urlPatterns = "/index")
 public class IndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-     
+
 	@EJB
 	private BrukerDAO brukerDAO;
-  
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		request.getRequestDispatcher("WEB-INF/jsp/index_ut.jsp").forward(request, response);
-	}
-
-	
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Bruker> DAOliste = brukerDAO.hentAlleBrukere();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		if (LoggInnUtil.erInnlogget(request)) {
-			request.setAttribute("DAOliste", DAOliste);
 			request.getRequestDispatcher("WEB-INF/jsp/index_in.jsp").forward(request, response);
 		} else {
-			response.sendRedirect("WEB-INF/jsp/index_ut.jsp");
+			request.getRequestDispatcher("WEB-INF/jsp/index_ut.jsp").forward(request, response);
 		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 }
