91fcdbec2f89689919aa41ebbe2307990c41c080
h595488 <jorgenvikan@gmail.com>
2022-04-04 12:42:34 +0200

    begynt å sette opp spilltest

---
+++ src/main/java/model/test/SpillTest.java
@@ -0,0 +1,49 @@
+package main.java.model.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
+
+//Får ikke testet om alt dette funker da Intellij hater klassestrukturen som dere har laget
+public class SpillTest {
+
+       /* Yatzyspill yatzyspill;
+        Poengtabell poengtabell;
+    @BeforeEach
+    public void reset(){
+
+        Bruker Erik = new Bruker();
+        Bruker Jorgen = new Bruker();
+        Bruker Eivind = new Bruker();
+        Bruker[] spillere = {Eivind,Erik,Jorgen};
+        yatzyspill = new Yatzyspill(1,Erik,spillere);
+        poengtabell = new Poengtabell(3);
+        yatzyspill.startSpill();
+    }
+
+    @Test
+    public void spillSattopp(){
+
+        assertEquals(Erik,yatzyspill.getAdmin());
+        assertEquals(yatzyspill.getSpillid(), 1);
+        assertEquals(yatzyspill.getSpillerSinTur(), Eivind);
+    }
+
+    @Test
+    public void testSpillrunde(){
+
+    }
+
+    @Test
+    public void testVinner(){
+        yatzyspill.setVinner(Erik);
+        yatzyspill.regnUtVinner();
+    }
+
+    @Test
+    public void testPurre(){}
+
+    @Test
+    public void testKick(){}*/
+}
